f300b6e2055fdb2a842a0ca66951ed4b65d76fc4
Leandro Lucas <leoViolinist22@gmail.com>
2023-02-10 14:58:10 -0300

    adicionando arquivo de reducao do json

---
+++ src/ReduzArquivoJsonResumo/Reduz.py
@@ -0,0 +1,4 @@
+# Código responsável por reduzir o tamanho total do arquivo de resumos json
+# Necessário o resultado da alocação do ano correspondente
+# Aval completo do relatório
+# Arquivo json de resumos
