6e88abd400119f4a9d4d3cde28ac8c50d89b4994
Leandro <leoviolinist22@gmail.com>
2023-03-29 17:35:02 -0300

    ajeitando o alinhamento de informacoes

---
+++ src/resumoOrientadores/alinha.py
@@ -0,0 +1,55 @@
+# Código responsável por alinhar na mesma sequencia o arquivo de resumo de orientadores com o criado com projetos
+import json
+
+# Abre arquivo de resumo dos orientadores
+def abre_orientadores(path_orientadores):
+
+    with open(path_orientadores) as file:
+        data = json.load(file)
+
+    return data
+
+
+def abre_projetos(path_projetos):
+    
+    with open(path_projetos) as file:
+        data = json.load(file)
+    
+    return data
+
+def main():
+    path_orientadores = "resumoOrientadores14.json"
+    path_projetos = "../CriadorDados/projetos2014.json"
+    resumo_nome = "resumoOrientadores14Alin.json"
+    orientadores_resumo = abre_orientadores(path_orientadores)
+    projetos = abre_projetos(path_projetos)
+
+    orientadores_resumo_alin = list()
+
+    counter = 0
+    # Vamos percorrer cada linha dos projetose dps percorrer cada linha de resumos dos meus orientadores
+    # Quando for achado o mesmo projeto no resumo, adicionamos uma nova linha de resumo id e adicionamos na lista
+    for projeto in projetos:
+        for resumo in orientadores_resumo:
+            if resumo['Projeto:'] == projeto['Projeto:']:
+                resumo['Id:'] = counter
+                orientadores_resumo_alin.append(resumo)
+                counter += 1 
+
+
+    # Responsável por criar o json do resumo dos orientadores
+    with open(resumo_nome, 'w') as resumo_js:
+        json.dump(orientadores_resumo_alin, resumo_js, indent = 4, ensure_ascii=False)
+    resumo_js.close()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
