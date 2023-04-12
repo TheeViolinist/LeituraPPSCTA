db6b6b6a13bda037ff10377be28409a9163f3e5e
Leandro Lucas <leoViolinist22@gmail.com>
2023-04-12 15:59:08 -0300

    arrumando o arquivo de alinhamento

--- src/resumoOrientadores/alinha.py
+++ src/resumoOrientadores/alinha.py
@@ -1,9 +1,11 @@
-# Código responsável por alinhar na mesma sequencia o arquivo de resumo de orientadores com o criado com projetos
+"""Código responsável por alinhar na mesma sequencia o arquivo de resumo 
+ de orientadores com o criado com projetos"""
 import json
+
 
 # Abre arquivo de resumo dos orientadores
 def abre_orientadores(path_orientadores):
-
+    """Retorna os dados do resumo dos orientadores"""
     with open(path_orientadores) as file:
         data = json.load(file)
 
@@ -11,44 +13,37 @@ def abre_orientadores(path_orientadores):
 
 
 def abre_projetos(path_projetos):
-    
+    """Retorna o dicionario dos projetos"""
     with open(path_projetos) as file:
         data = json.load(file)
-    
+
     return data
+
 
 def main():
     path_orientadores = "resumoOrientadores14.json"
-    path_projetos = "../CriadorDados/projetos2014.json"
+    path_projetos = "../CriadorDadosModelo/projetos2014.json"
     resumo_nome = "resumoOrientadores14Alin.json"
     orientadores_resumo = abre_orientadores(path_orientadores)
     projetos = abre_projetos(path_projetos)
 
     orientadores_resumo_alin = list()
 
-    counter = 0
-    # Vamos percorrer cada linha dos projetose dps percorrer cada linha de resumos dos meus orientadores
-    # Quando for achado o mesmo projeto no resumo, adicionamos uma nova linha de resumo id e adicionamos na lista
+    # Vamos percorrer cada linha dos projetos e dps percorrer cada linha
+    # de resumos dos meus orientadores
+    # Quando for achado o mesmo projeto no resumo, adicionamos
+    # uma nova linha de resumo id e adicionamos na lista
     for projeto in projetos:
         for resumo in orientadores_resumo:
             if resumo['Projeto:'] == projeto['Projeto:']:
-                resumo['Id:'] = counter
+                resumo['Id:'] = projeto["id"]
                 orientadores_resumo_alin.append(resumo)
-                counter += 1 
 
 
     # Responsável por criar o json do resumo dos orientadores
     with open(resumo_nome, 'w') as resumo_js:
-        json.dump(orientadores_resumo_alin, resumo_js, indent = 4, ensure_ascii=False)
+        json.dump(orientadores_resumo_alin, resumo_js, indent=4, ensure_ascii=False)
     resumo_js.close()
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
